541c3118e261fd88b035725cf8810bc0854a78b0
jamesndungu <jimmindungu3@gmail.com>
2023-09-29 06:30:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+// To pass the tests, don't forget to return your fetch!
+fetch("https://anapioficeandfire.com/api/books")
+return fetch("https://anapioficeandfire.com/api/books")
+.then(function (response) {
+  return response.json();
+})
+.then (function (data) {
+  renderBooks(data)
+})
 }
 
 function renderBooks(books) {
